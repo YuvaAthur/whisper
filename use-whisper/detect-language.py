5ecd2fe7403dae255b67731971e46e4f5c4d8f3c
Yuvaraj <yuvaraj.a.r@gmail.com>
2026-04-23 22:39:18 -0700

    More experiments - bg gets to best result on language detection

--- use-whisper/detect-language.py
+++ use-whisper/detect-language.py
@@ -5,7 +5,9 @@ from operator import itemgetter
 model = whisper.load_model("tiny") #turbo 
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("ss-audio-may-23.mp3") #sanskrit audio
+# audio = whisper.load_audio("ss-audio-may-23.mp3") #sanskrit audio
+# audio = whisper.load_audio("Atharvaveda_Kanda_10_0001.wav") #sanskrit audio
+audio = whisper.load_audio("bg-chap1.mp3") #background audio
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
@@ -19,7 +21,7 @@ probs_sorted = sorted(probs.items(),  key=itemgetter(1), reverse=True) # type: i
 # probs_sorted = sorted(probs,key=probs.get, reverse=True) # type: ignore
 print(f"Detected language: {max(probs, key=probs.get)}") # type: ignore
 
-# output the language probabilities
+# output the language probabilities :ss-audio-may-23.mp3 
     # ('si', 0.5062861442565918) : Sinhala
     # ('hi', 0.10614561289548874) : Hindi
     # ('pa', 0.042809389531612396) : Punjabi
@@ -27,6 +29,11 @@ print(f"Detected language: {max(probs, key=probs.get)}") # type: ignore
     # ('kn', 0.04115227237343788) : Kannada
     # ('sa', 0.03515150025486946) : Sanskrit
 
+
+# output the language probabilities :bg-chap1.mp3 
+    # ('sa', 0.22502821683883667) : Sanskrit
+    # ('en', 0.16907507181167603) : English
+    # ('lv', 0.10124222934246063) : Latvian
 
 # # decode the audio
 # options = whisper.DecodingOptions()
